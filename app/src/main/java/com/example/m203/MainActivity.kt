8997bba334a3bfae34c46e6c19348c1681732d1e
omar OUGHZAL <omar.oughzal@gmail.com>
2025-10-18 20:44:34 +0100

    Initial Commit

--- app/src/main/java/com/example/m203/MainActivity.kt
+++ app/src/main/java/com/example/m203/MainActivity.kt
@@ -9,14 +9,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.m203.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
