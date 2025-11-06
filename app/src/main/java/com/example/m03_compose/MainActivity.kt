2ad93866ec5111e842fdde8f2652bdfceceb2aa3
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-06 03:07:07 +0100

    Commit

--- app/src/main/java/com/example/m03_compose/MainActivity.kt
+++ app/src/main/java/com/example/m03_compose/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,9 +43,12 @@ fun MainScreen() {
             .fillMaxSize()
             .background(Color.White)
     ) {
-        Text(
-            text = "Hello World"
+        Button(
+        onClick = {}
         )
+        {
+            Text(text = "Hello World!")
+        }
     }
 }
 
