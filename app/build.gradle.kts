8d46526c93ba30b462a1d62f9578b845ca8d3dde
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-14 00:00:02 +0100

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.compose.material.icons.extended)
+
+    implementation(libs.androidx.navigation.compose)
 }
