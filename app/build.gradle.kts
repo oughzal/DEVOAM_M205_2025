dfdca978b4a6c6bcdde2a341e706c7e9fdd77ccb
omar OUGHZAL <omar.oughzal@gmail.com>
2025-11-21 01:23:29 +0100

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
 }
 
 android {
@@ -60,5 +60,5 @@ dependencies {
     implementation(libs.compose.material.icons.extended)
 
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.serialization.json.v160)
 }
