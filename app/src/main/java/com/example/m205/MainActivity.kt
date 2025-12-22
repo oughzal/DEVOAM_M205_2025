9fbf3f3e38e90b0d93ef649f60f716ae2e768d8f
omar OUGHZAL <omar.oughzal@gmail.com>
2025-12-22 13:50:48 +0100

    commit

--- app/src/main/java/com/example/m205/MainActivity.kt
+++ app/src/main/java/com/example/m205/MainActivity.kt
@@ -35,102 +35,12 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                AlarmTimePicker(this@MainActivity)
+
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AlarmTimePicker(context : Context) {
-    val now = Calendar.getInstance()
-    val timePickerState = rememberTimePickerState(
-        initialHour = now.get(Calendar.HOUR_OF_DAY),
-        initialMinute = now.get(Calendar.MINUTE),
-        is24Hour = true
-    )
-
-    Column(modifier = Modifier.padding(16.dp)) {
-
-        TimePicker(state = timePickerState)
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {
-            val intent = Intent(context, AlarmReceiver::class.java).apply {
-                action = "ACTION_ALARM_RING"
-            }
-
-            val pendingIntent = PendingIntent.getBroadcast(
-                context,
-                0,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
-            val triggerTime = buildAlarmTime(
-                timePickerState.hour,
-                timePickerState.minute
-            )
-            scheduleExactAlarm(context, triggerTime, pendingIntent)
-
-        }) {
-            Text("Programmer l’alarme")
-        }
-        Button(onClick = {
-            val intent = Intent(context, AlarmReceiver::class.java).apply {
-                action = "ACTION_ALARM_RING"
-            }
-
-            val pendingIntent = PendingIntent.getBroadcast(
-                context,
-                0,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
-
-            val alarmManager =
-                context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-            alarmManager.cancel(pendingIntent)
-
-        }) {
-            Text("Annuler l’alarme")
-        }
-    }
-}
-
-
-fun buildAlarmTime(hour: Int, minute: Int): Long {
-
-    val calendar = Calendar.getInstance().apply {
-        set(Calendar.HOUR_OF_DAY, hour)
-        set(Calendar.MINUTE, minute)
-        set(Calendar.SECOND, 0)
-
-        if (timeInMillis <= System.currentTimeMillis()) {
-            add(Calendar.DAY_OF_MONTH, 1)
-        }
-    }
-
-    return calendar.timeInMillis
-}
-
-fun scheduleExactAlarm(
-    context: Context,
-    triggerTime: Long,
-    pendingIntent: PendingIntent
-) {
-
-    val alarmManager =
-        context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-    alarmManager.setExactAndAllowWhileIdle(
-        AlarmManager.RTC_WAKEUP,
-        triggerTime,
-        pendingIntent
-    )
-}
 
 
 
