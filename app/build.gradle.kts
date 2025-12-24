c9045a06c07be1bece5cee392c0bc14fe7412e4f
omar OUGHZAL <omar.oughzal@gmail.com>
2025-12-24 14:57:22 +0100

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.play.services.location)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.maps.android:maps-compose:4.3.0")
 }
--- app/src/main/java/com/example/m205/MainActivity.kt
+++ app/src/main/java/com/example/m205/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.m205
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
@@ -20,9 +21,24 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.room.util.copy
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
@@ -30,17 +46,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             Column(
-                modifier = Modifier.fillMaxSize().padding(16.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-
+                currentLocationMap()
             }
         }
     }
 }
 
+@SuppressLint("MissingPermission")
+fun getLocation(context: Context,onSucces :(LatLng)-> Unit){
 
+    val location = LocationServices.getFusedLocationProviderClient(context)
+
+    location.lastLocation
+        .addOnSuccessListener { location ->
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                onSucces(LatLng(location.latitude, location.longitude))
+            }
+        }
+}
+@Composable
+fun currentLocationMap(){
+    val context = LocalContext.current
+    var userLocation by remember{ mutableStateOf<LatLng?>(null) }
+    val cameraPositionState = rememberCameraPositionState()
+    LaunchedEffect(Unit) {
+        getLocation(context) { latLng ->
+            userLocation = latLng
+            cameraPositionState.position = CameraPosition.fromLatLngZoom(latLng, 20f)
+        }
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        userLocation?.let {
+            Marker(
+                state = MarkerState(position = it),
+                title = "Ma position"
+            )
+        }
+    }
+}
 
 
