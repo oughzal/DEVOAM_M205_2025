ff938ce5d00b62d8e49a1c82d59b53b714297f22
omar OUGHZAL <omar.oughzal@gmail.com>
2025-12-28 23:35:32 +0100

    commit

--- app/src/main/java/com/example/m205/MainActivity.kt
+++ app/src/main/java/com/example/m205/MainActivity.kt
@@ -1,28 +1,46 @@
 package com.example.m205
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
@@ -35,11 +53,94 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-
+                ContactsScreen()
             }
         }
     }
 }
+
+@Composable
+fun ContactsScreen() {
+
+    val context = LocalContext.current
+  val viewModel: ContactsViewModel = viewModel()
+
+    var permissionGranted by remember { mutableStateOf(false) }
+
+    val permissionLauncher =
+        rememberLauncherForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            permissionGranted = isGranted
+            if (isGranted) {
+                viewModel.loadContacts(context)
+            }
+        }
+
+    LaunchedEffect(Unit) {
+        if (
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionGranted = true
+            viewModel.loadContacts(context)
+        } else {
+            permissionLauncher.launch(
+                Manifest.permission.READ_CONTACTS
+            )
+        }
+    }
+
+    if (permissionGranted) {
+        ContactsList(viewModel.contacts)
+    } else {
+        PermissionDeniedMessage()
+    }
+}
+
+@Composable
+fun ContactsList(contacts: List<Contact>) {
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(contacts) { contact ->
+            ContactItem(contact)
+        }
+    }
+}
+
+@Composable
+fun ContactItem(contact: Contact) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(text = contact.name)
+        Text(text = contact.phone)
+    }
+
+   HorizontalDivider()
+}
+
+@Composable
+fun PermissionDeniedMessage() {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Permission Contacts refusée")
+    }
+}
+
+
+
+
 
 
 
