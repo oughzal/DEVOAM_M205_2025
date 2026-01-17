a7e9e1a797464da9b3ab49a03d711e0155f1978b
omar OUGHZAL <omar.oughzal@gmail.com>
2026-01-17 03:03:29 +0100

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
 }
 
 dependencies {
